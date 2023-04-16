8469acfa1a7cb22b1bf05d306764222eae6fbf94
chrischile <117464265+clohmeie@users.noreply.github.com>
2023-04-16 11:27:30 -0500

    Version with simple image

--- src/Garden.java
+++ src/Garden.java
@@ -12,15 +12,15 @@ private World world;
         canvas = new CanvasWindow("Sanity Garden", 700, 700);
         //worldMap.setImagePath("res/Map of The Shimmering Sea.jpg");
 
-        world = new World();
+        //world = new World();
         //world.setWorldImage(worldMap);
-        world.setWorldImage();
-        canvas.add(world.getWorldImage(), 0, 0);
+        //world.setWorldImage();
+        //canvas.add(world.getWorldImage(), 0, 0);
         // Rectangle rect = new Rectangle(0, 0, 50, 50);
         // world.add(rect, 0, 0);
         // canvas.add(world, 0, 0);
-        //Image image = new Image(0, 0, "Map.jpg");
-        //canvas.add(image);
+        Image image = new Image(0, 0, "Map.jpg");
+        canvas.add(image);
 
     }
 
