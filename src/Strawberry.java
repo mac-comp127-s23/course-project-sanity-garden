78507ad5b2e436113fc7c28fc7aa163142079fde
lvescovo <lvescovo@macalester.edu>
2023-04-21 11:37:02 -0500

    perhaps a strawberry class?

--- src/Strawberry.java
+++ src/Strawberry.java
@@ -1,3 +1,20 @@
+import edu.macalester.graphics.Image;
+
 public class Strawberry {
-    
+public int howMany = 0;
+public Image strawberryBud;
+
+
+
+    public Strawberry(Image strawberryBud) {
+
+        strawberryBud = new Image(0, 0, "strawberryBud.jpg");
+ 
+    }
+
+    public Image getStrawberry() {
+        return this.strawberryBud;
+    }
 }
+
+
