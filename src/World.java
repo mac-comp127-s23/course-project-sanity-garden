7a62a90fba65a1e25e9dfb924ce30af4b397d622
chrischile <117464265+clohmeie@users.noreply.github.com>
2023-04-21 11:19:34 -0500

    Changed base map

--- src/World.java
+++ src/World.java
@@ -5,9 +5,7 @@ public class World extends GraphicsGroup {
     private Image worldMap;
 
     public World() {
-        worldMap = new Image(0, 0, "Map.jpg");
-        worldMap.setMaxHeight(700);
-        worldMap.setMaxWidth(700);
+        worldMap = new Image(0, 0, "Map.png");
         add(worldMap);
     }
 
