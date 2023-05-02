96baae9a15ef1afef40ce0f9b701de90df323f27
enguyen120 <84419638+enguyen120@users.noreply.github.com>
2023-05-02 12:41:03 -0500

    scaling straw

--- src/Strawberry.java
+++ src/Strawberry.java
@@ -26,17 +26,13 @@ private static Item strawberryItem = new Item("Strawbery", 1.5, 20);
         seed.setMaxHeight(seed.getImageHeight() * 0.08);
 
         Image sprout = new Image("realsprout.png");
-        sprout.setMaxWidth(sprout.getImageWidth() * 0.001);
-        sprout.setMaxHeight(sprout.getImageHeight() * 0.001);
+        sprout.setScale(0.1, 0.09);
 
-        Image strawberry = new Image("strawberry.png");
-        sprout.setMaxWidth(sprout.getImageWidth() * 0.5);
-        sprout.setMaxHeight(sprout.getImageHeight() * 0.5);
 
 
         plantImages.add(seed);
         plantImages.add(sprout);
-        plantImages.add(strawberry);
+        plantImages.add(new Image("strawberry.png"));
         
  
     }
